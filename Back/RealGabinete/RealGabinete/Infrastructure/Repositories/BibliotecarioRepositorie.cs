50a652bb8f053fc1538c1c2b239940a14e663bd9
Rafael Velloso <rafavelloso2@gmail.com>
2026-07-29 23:16:04 +0100

    feat: adiciona repositórios de bibliotecários e leitores

---
+++ Back/RealGabinete/RealGabinete/Infrastructure/Repositories/BibliotecarioRepositorie.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using RealGabinete.Domain.Entities;
+using RealGabinete.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using RealGabinete.Domain.Interfaces;
+
+namespace RealGabinete.Infrastructure.Repositories 
+{
+    public class BibliotecarioRepositorie : IBibliotecarioRepository
+    {
+        private readonly RealGabineteContext _context;
+
+        public BibliotecarioRepositorie(RealGabineteContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Bibliotecario>> ObterTodosAsync()
+        {
+            return await _context.Bibliotecarios.ToListAsync();
+        }
+
+        public async Task<Bibliotecario?> ObterPorIdAsync(int id)
+        {
+            return await _context.Bibliotecarios.FindAsync(id);
+        }
+
+        public async Task<Bibliotecario?> AdicionarAsync(Bibliotecario bibliotecario)
+        {
+            _context.Bibliotecarios.Add(bibliotecario);
+            await _context.SaveChangesAsync();
+            return bibliotecario;
+        }
+
+        public async Task<Bibliotecario?> AtualizarAsync(Bibliotecario bibliotecario)
+        {
+            _context.Bibliotecarios.Update(bibliotecario);
+            await _context.SaveChangesAsync();
+            return bibliotecario;
+        }
+
+       
+    }
+}
--- Back/RealGabinete/RealGabinete/Infrastructure/Repositories/ExemplaresRepositorie.cs
+++ Back/RealGabinete/RealGabinete/Infrastructure/Repositories/ExemplaresRepositorie.cs
@@ -40,12 +40,12 @@ namespace RealGabinete.Infrastructure.Repositories
         {
             _context.Exemplares.Update(exemplar);
             await _context.SaveChangesAsync();
-            
+
         }
 
-        public async Task RemoverAsync(int id)
+        public async Task RemoverAsync(int Id)
         {
-            var exemplar = await _context.Exemplares.FindAsync(id);
+            var exemplar = await _context.Exemplares.FindAsync(Id);
             if (exemplar != null)
             {
                 _context.Exemplares.Remove(exemplar);
@@ -54,3 +54,4 @@ namespace RealGabinete.Infrastructure.Repositories
         }
     }
 }
+        
